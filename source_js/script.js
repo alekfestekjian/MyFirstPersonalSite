564898fa9520862c50512467def2fffe8f569453
Alek Festekjian <festekj2@illinois.edu>
2016-02-13 15:25:02 -0600

    pls god work

--- source_js/script.js
+++ source_js/script.js
@@ -1,10 +1,4 @@
-// var divs = document.getElementsByTagName('div');
-// for(var i=0; i<divs.length; i++) {
-//   divs[i].addEventListener("click", highlightThis);
-//   /*
-//   divs[i].addEventListener("click", highlightThis, true);
-//   divs[i].addEventListener("click", highlightThis, false);*/
-// }
+
 
 function highlightThis(event) {
     //event.stopPropagation();
@@ -19,7 +13,6 @@ function highlightThis(event) {
 http://www.bootply.com/109943
 shrinking nav bar
 */
-
 $(window).scroll(function() {
   if ($(document).scrollTop() > 50) {
     $('nav').addClass('shrink');
@@ -28,17 +21,44 @@ $(window).scroll(function() {
   }
 });
 $(document).scroll(function(){
+  //Change nav bar anchor tags to classes
   $('a[href^="#"]').on('click',function (e) {
-      e.preventDefault();
-
+    //get child hash
       var target = this.hash;
       var $target = $(target);
-      console.log($target.offset().top)
+      if(this.hash == "#close" || this.hash == "#MasteryModal" || this.hash == "#ArmadaModal" || this.hash == "#FastModal"){
+        return;
+      }
+    
 
+      e.preventDefault();
+   
       $('html, body').stop().animate({
           'scrollTop': $target.offset().top - 40
       }, 900, 'swing', function () {
           window.location.hash = target;
       });
+   
   });
+});
+/*Position derived from*/
+/*https://api.jquery.com/hover/*/
+/*http://stackoverflow.com/questions/23206101/navigation-bar-that-tells-where-you-are*/
+function onScreen() {
+    // Check if the top of the page collides with each section
+    $('section').each(function() {
+        var windowScroll = $(document).scrollTop();
+        var navHeight = $('.nav-bar').height();
+  
+        if( windowScroll + navHeight >= $(this).offset().top && windowScroll + navHeight < $(this).offset().top + $(this).height()) {
+            $('.nav-bar ul a#' + $(this).attr('id')+'_button').addClass('highlight');
+        } else {
+            $('.nav-bar ul a#' + $(this).attr('id')+'_button').removeClass('highlight');
+        }
+
+    });
+}
+
+$(window).on('scroll resize', function () {
+    onScreen();
 });
